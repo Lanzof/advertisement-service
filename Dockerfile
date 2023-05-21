12d11a2a6331abdd8e47158769a69e25ee205499
Pokotilov Sergey <sercam@bk.ru>
2023-05-22 00:25:23 +0400

    docker work

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+
+# creates a layer from the openjdk:17-alpine Docker image.
+FROM openjdk:17-alpine
+
+# cd /app
+WORKDIR /app
+
+# Refer to Maven build -> finalName
+ARG JAR_FILE=target/final-task-*.jar
+
+# cp target/spring-boot-docker-0.0.1-SNAPSHOT.jar /app/spring-boot-docker.jar
+COPY ${JAR_FILE} final-task.jar
+
+# java -jar /app/spring-boot-docker.jar
+CMD ["java", "-jar", "-Xmx1024M", "/app/final-task.jar"]
+
+# Make port 8090 available to the world outside this container
+EXPOSE 8080
--- src/main/java/com/pokotilov/finaltask/configs/SecurityConfig.java
+++ src/main/java/com/pokotilov/finaltask/configs/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.pokotilov.finaltask.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -27,6 +28,7 @@ public class SecurityConfig {
             .csrf().disable()
             .authorizeHttpRequests()
             .requestMatchers("/api/auth/**").permitAll()
+            .requestMatchers(HttpMethod.GET,"/api/advert/**").permitAll()
             .requestMatchers("/swagger-ui.html", "/v3/api-docs/**", "/swagger-ui/**").permitAll()
             .anyRequest().authenticated()
             .and()
